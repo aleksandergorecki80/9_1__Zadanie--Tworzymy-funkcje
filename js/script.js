aa8174fa96a32488e8f5c401c473d1b9925ff53c
Aleksander Gorecki <aleksandergorecki80@gmail.com>
2018-04-13 09:07:05 +0100

    Update js file

--- js/script.js
+++ js/script.js
@@ -4,21 +4,26 @@ var
 	triangle3Area; 
  
 
-function getTriangleArea (a, h){
-	
-var resoult = a > 0 && h>0 ? a*h/2 : 'Nieprawidłowe dane.';
-	
-/*	
-	if (a>0 && h>0){
-		return a*h/2;
-	}
-	else{
-		console.log('Nieprawidłowe dane');
-	}
-*/
-	
+function getTriangleArea (a, h){	
+	var resoult = a > 0 && h>0 ? a*h/2 : 'Nieprawidłowe dane.';
 	return resoult;
 }
 
 
 console.log(getTriangleArea (10, 20));
+
+triangle1Area = getTriangleArea(10, 20);
+triangle2Area = getTriangleArea(5, -10);
+triangle3Area = getTriangleArea(6, 16.5);
+
+
+ document.write(
+				'Pole 1: ' + triangle1Area + '<br>'
+			+	'Pole 2: ' + triangle2Area + '<br>'
+			+	'Pole 3: ' + triangle3Area + '<br>'
+			); 
+ 
+ 
+ 
+ 
+ 
